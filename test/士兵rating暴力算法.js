51fe2309d3b2c2c315f427a8333c78377c8eec76
zzc <791827624@qq.com>
2020-08-04 14:16:45 +0800

    0804 middle

--- test/士兵rating暴力算法.js
+++ test/士兵rating暴力算法.js
@@ -1,14 +1,20 @@
+/**
+ * @param {number[]} rating
+ * @return {number}
+ */
 var numTeams = function(rating) {
     let num = 0
     for(let i = 0; i < rating.length - 2; i++) {
-        for(let j =i + 1; j < rating.length - i + 1; j++) {
+        for(let j = i + 1; j < rating.length  - 1; j++) {
             for(let k = rating.length - 1; k > j; k --) {
-                if(rating[i] > rating[j] && rating[j] > rating[k]){
-                    console.log(i,j,k)
+                if(rating[i] > rating[j] && rating[j] > rating[k]) {
+                    num++
+                }
+                if(rating[i] < rating[j] && rating[j] < rating[k]) {
+                    num++
                 }
             }
         }
     }
-    <!-- return num -->
+    return num
 };
-numTeams([2,5,3,4,1])
